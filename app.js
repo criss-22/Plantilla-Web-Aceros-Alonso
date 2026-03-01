eb0b7baf2de3a01fc4d358fabac9b4c644cbdb7e
Cristopher Osorio Morales <20241042@uthh.edu.mx>
2026-02-28 18:12:23 -0600

    se arreglo

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (btnMenu && menuPrincipal) {
         btnMenu.addEventListener('click', () => {
             menuPrincipal.classList.toggle('hidden');
-            console.log("🍔 Menú hamburguesa presionado.");
         });
     }
 
